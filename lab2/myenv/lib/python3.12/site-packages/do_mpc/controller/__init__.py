ce714be2c07644c8db8f3e0611fc72ca7e8ba967
Willmer Wohlén <willmer@wohlen.se>
2026-09-09 15:21:58 +0000

    lab2 fix

--- lab2/myenv/lib/python3.12/site-packages/do_mpc/controller/__init__.py
+++
@@ -1,7 +0,0 @@
-
-"""
-Controller for dynamic systems.
-"""
-
-from ._mpc import MPC,MPCSettings
-from ._lqr import LQR,LQRSettings
--- lab2/myenv/lib/python3.12/site-packages/do_mpc/differentiator/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tools for NLP differentiation.
-
-Warning:
-    This is an experimental feature. The API might change in the future.
-"""
-
-from ._nlpdifferentiator import NLPDifferentiator, DoMPCDifferentiator
-from . import helper
